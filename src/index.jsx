4a906e53e26b7350767ee3c29f2e86c6c53bb699
nttu94507 <nttu94507@gmail.com>
2020-05-19 18:57:14 +0800

    prop-types

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState,useEffect }  from 'react';
 import ReactDom from 'react-dom';
 import Styles from './index.scss';
+import PropTypes from 'prop-types';
 
  
 const Counter = () =>{
@@ -34,7 +35,8 @@ const Main = () =>{
 const HelloWorld = (props) => {
     const { names } = props;
     const isEmpty = value => value === ''
-    return names.map(name=>(
+    
+    return (
         <div
             key={name}
             className={
@@ -44,11 +46,25 @@ const HelloWorld = (props) => {
             style={{
                 'font-size': 28,
             }}
-            >
-               {`Hello ${isEmpty(name) ? 'World' : name}!`} 
-            </div>
-    ))
-}
+            > 
+            {names.map(name=>(
+                <div
+                    key={name}
+                    classname={isEmpty(name) ? '' : Styles.Main}    
+                >
+                    {`Hello ${isEmpty(name) ? 'World' : name}!`} 
+                </div>
+            ))}
+        </div>
+    )
+};
+HelloWorld.propTypes = {
+    names: PropTypes.arrayOf(PropTypes.string),
+  };
+
+  HelloWorld.defaultProps = {
+    names: ['Default string'],
+  };
 
 
-ReactDom.render(<Main/> , document.getElementById('root'));
+ReactDom.render(<HelloWorld names={[1,2,3]}/> , document.getElementById('root'));

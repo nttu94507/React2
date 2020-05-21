fc0bc80ecc8354c56d2bec6e5cab0075b9cc35b7
nttu94507 <nttu94507@gmail.com>
2020-05-21 18:42:12 +0800

    useContext

--- src/index.jsx
+++ src/index.jsx
@@ -3,68 +3,101 @@ import ReactDom from 'react-dom';
 import Styles from './index.scss';
 import PropTypes from 'prop-types';
 
- 
-const Counter = () =>{
-    const [count, setCount] = useState(0)
-    useEffect(() => {
-        return () => {
-            console.log('cpmponent 移除後')
-        }
-    },[])
-    return (
-        <>
-            <h1 className={Styles.main}>{count}</h1>
-            <button type="button"onClick={()=>{setCount(count + 1)}} >點我</button>
-        </>
-    )
-} 
 
-const Main = () =>{
-    const[hiddenCounter, setHiddenCounter] = useState(false)
-    return (
-        <>
-            <button type="button" onClick={() => {
-                setHiddenCounter(!hiddenCounter);}}>
-                關閉計數器
-            </button>
-            {hiddenCounter ? null:<Counter />}
-        </>
+const Task = (props) => {
+    const { task } = props
+    return <div>{task}</div>
+}
+
+Task.PropTypes ={
+    task: PropTypes.string,
+}
+
+Task.defailtProps = {
+    task: '',
+}
+
+const Todolist = (props) => {
+    const{todolist} = props
+    return todolist.map(task => (
+        <ul key={task}>
+            <Task task={task} />
+        </ul>
+    ))
+}
+
+const Main = () => {
+    const [todolist] = useState(['first','second',"1"])
+    return(
+        <div>
+            <span>{`代辦事項數:${todolist.length}`}</span>
+            <Todolist todolist={todolist} />
+        </div> 
     )
 }
+
+
+// const Counter = () =>{
+//     const [count, setCount] = useState(0)
+//     useEffect(() => {
+//         return () => {
+//             console.log('cpmponent 移除後')
+//         }
+//     },[])
+//     return (
+//         <>
+//             <h1 className={Styles.main}>{count}</h1>
+//             <button type="button"onClick={()=>{setCount(count + 1)}} >點我</button>
+//         </>
+//     )
+// } 
+
+// const Main = () =>{
+//     const[hiddenCounter, setHiddenCounter] = useState(false)
+//     return (
+//         <>
+//             <button type="button" onClick={() => {
+//                 setHiddenCounter(!hiddenCounter);}}>
+//                 關閉計數器
+//             </button>
+//             {hiddenCounter ? null:<Counter />}
+//         </>
+//     )
+// }
     
-const HelloWorld = (props) => {
-    const { names } = props;
-    const isEmpty = value => value === ''
+// const HelloWorld = (props) => {
+//     const { names } = props;
+//     const isEmpty = value => value === ''
     
-    return (
-        <div
-            key={name}
-            className={
-                `${Styles.mainBackground} 
-                ${isEmpty(name) ? '' : (Styles.main)}`
-            }
-            style={{
-                'font-size': 28,
-            }}
-            > 
-            {names.map(name=>(
-                <div
-                    key={name}
-                    classname={isEmpty(name) ? '' : Styles.Main}    
-                >
-                    {`Hello ${isEmpty(name) ? 'World' : name}!`} 
-                </div>
-            ))}
-        </div>
-    )
-};
-HelloWorld.propTypes = {
-    names: PropTypes.arrayOf(PropTypes.string),
-  };
+//     return (
+//         <div
+//             key={name}
+//             className={
+//                 `${Styles.mainBackground} 
+//                 ${isEmpty(name) ? '' : (Styles.main)}`
+//             }
+//             style={{
+//                 'font-size': 28,
+//             }}
+//             > 
+//             {names.map(name=>(
+//                 <div
+//                     key={name}
+//                     classname={isEmpty(name) ? '' : Styles.Main}    
+//                 >
+//                     {`Hello ${isEmpty(name) ? 'World' : name}!`} 
+//                 </div>
+//             ))}
+//         </div>
+//     )
+// };
+// HelloWorld.propTypes = {
+//     names: PropTypes.arrayOf(PropTypes.string),
+//   };
 
-  HelloWorld.defaultProps = {
-    names: ['Default string'],
-  };
+//   HelloWorld.defaultProps = {
+//     names: ['Default string'],
+//   };
 
 
-ReactDom.render(<HelloWorld names={[1,2,3]}/> , document.getElementById('root'));
+ReactDom.render(<Main /> , document.getElementById('root'));

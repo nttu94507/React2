948dfc479c31b66e65ed918c7bae606d08f8a481
nttu94507 <nttu94507@gmail.com>
2020-07-07 14:54:19 +0800

    sort out Main to component

---
+++ src/component/Main/Main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Switch, Route ,Link} from 'react-router-dom'
+import {Content} from '../Content'
+import {Todolist} from '../TodoList'
+import {List} from '../List'
+import {Redirect} from '../Redirect'
+
+const Main =() =>(
+    <div>
+        <ul>
+            <li><Link to='/'>首頁</Link></li>
+            <li><Link to='/todolist'>待辦事項</Link></li>
+            <li><a href='http://192.168.10.186:8001'>宜蘭縣政府</a></li>
+
+        </ul>
+        <Switch>
+            <Route exact path='/' component={Content} />
+            <Route path='/todolist' component={Todolist} />
+            <Route path='/list/:taskName' component={List} />
+            <Route path='/Yilan' component={Redirect} loc='http://192.168.10.186:8001'/>
+        </Switch>
+    </div>
+)
+
+export default Main
--- src/index.jsx
+++ src/index.jsx
@@ -2,23 +2,7 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {Provider} from 'react-redux'
 import store from './store'
-import Content from './component/Content';
-import TodoList from './component/TodoList';
-import { Route, Switch,Link,BrowserRouter,HashRouter} from 'react-router-dom';
-import List from './component/List';
+import { HashRouter} from 'react-router-dom';
+import Main from './component/Main'
 
-
-
-const Main = () => (
-    <div>
-        <Link to="/">首頁</Link>
-        <Link to="/todolist">待辦事項</Link>
-        <hr></hr>
-        <Switch>
-            <Route exact path="/" component ={Content}/>
-            <Route  path="/todolist" component ={TodoList}/>
-            <Route  path="/list/:taskName" component ={List}/>
-        </Switch>
-    </div>
-)
 ReactDom.render(<Provider store={store}><HashRouter><Main /></HashRouter></Provider> , document.getElementById('root'));

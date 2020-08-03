68a41a1e9654fcc8313e28dd71baae44edaaa3d8
nttu94507 <nttu94507@gmail.com>
2020-08-03 16:50:15 +0800

    scss export to tS

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import {Provider} from 'react-redux'
 import store from './store'
-import { HashRouter} from 'react-router-dom';
+import { HashRouter} from 'react-router-dom'
 import Main from './component/Main'
+import styles from './index.scss'
 
-ReactDom.render(<Provider store={store}><HashRouter><Main /></HashRouter></Provider> , document.getElementById('root'));
+ReactDom.render(
+    <Provider store={store}>
+        <HashRouter>
+            <div className={styles.mainBlock}>
+                <Main />
+            </div>
+        </HashRouter>
+    </Provider> 
+, document.getElementById('root'));
